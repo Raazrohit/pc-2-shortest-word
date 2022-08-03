cf4482730b974e4d71d622cb69566641f44dbd4d
Rohit Raj <rohit2000lmp@gmail.com>
2022-08-03 10:33:55 +0530

    s5pc2

--- src/main/java/com/jap/ShortestWord.java
+++ src/main/java/com/jap/ShortestWord.java
@@ -3,18 +3,24 @@ package com.jap;
 
 public class ShortestWord {
     public static void main(String[] args) {
-
-        //Declare and initialize a String array.
-
+	 //Declare and initialize a String array.
+	String words[] = {"java", "programming", "language"};
         // Create an object of the class ShortestWord
-
+	ShortestWord object = new ShortestWord();
         //Call the method findShortestWord() and pass the array.
+		String shortestword = object.findShortestWord(words);
+		System.out.println("Shortest word from given array of string is : "+ shortestword);
     }
 
     //Write the logic to find the shortest word from the String array inside
     // the below method and return the shortest word.
     public String findShortestWord(String[] words) {
-
-        return "";
+	String shortestString = words[0];
+	int n = words.length;
+		for(int i = 0;i < n; i++){
+			if(words[i].compareTo(shortestString) < 0)
+				shortestString = words[i];
+		} 
+        return shortestString;
     }
 }
